bae1a71c27d760fe09e4a7eb010fdac3bd00b159
CokerAlcocer <20203tn066@utez.edu.mx>
2022-02-11 09:06:02 -0600

    xd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { CategoryScreen } from './components/category/CategoryScreen';
+import { SubCatgoryScreen } from './components/subcategory/SubCatgoryScreen';
+import FeatherIcon from "feather-icons-react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+          <Navbar.Brand href="#home">
+            <FeatherIcon icon="home"/>
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Link to={"/"} className='nav-link'>Categorías</Link>
+            <Link to={"/subcategory"} className='nav-link'>Sub Categorías</Link>
+          </Nav>
+          </Container>
+        </Navbar>
+        <Container>
+          <Routes>
+            <Route path={"/"} element={<CategoryScreen/>} />
+            <Route path="/subcategory" element={<SubCatgoryScreen/>} />
+          </Routes>
+        </Container>
+      </Router>
+    </>
   );
 }
 
